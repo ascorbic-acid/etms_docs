8326cb3e6adf116cc5a94411eafb0eea40c9e49f
Imad Abdou <igentle.appletec@gmail.com>
2025-02-23 08:00:54 +0000

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,7 @@
+<template>
+    <div style="display: flex; align-items: center;">
+      <img src="~/assets/etms_logo.png" style="width: 45px; height: 45px;" />
+      <span style="width: 10px;"></span>
+      <h3 >ETMS Docs for Newbies</h3>
+    </div>
+  </template>
